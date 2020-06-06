4d01418ab979088e32e90113e6ee1df327df52d8
Sergei Chirkov <sergei_chirkov@epam.com>
2020-06-07 01:14:12 +0400

    Added Demo file

--- src/test/java/Runner/Runner.java
+++ src/test/java/Runner/Runner.java
@@ -11,7 +11,7 @@ import io.cucumber.junit.CucumberOptions;
 		plugin = {"json:target/cucumber/cucumber-json-report.json", "BaseTest.BaseTest"},
 		glue = {"StepDefs", "BaseTest"},
 		strict = true,
-		tags = "@UUIDTest"
+		tags = "@Regression"
 )
 public class Runner {
 }
